51e2378a9f03f5d9d64b1e2aa95a5150ab9a5201
abhinavchawla13 <abhinavchawla13@yahoo.com>
2015-11-10 16:18:06 -0500

    Coordinates update with location changes

--- app/src/main/java/com/philips/lighting/quickstart/MyApplicationActivity.java
+++ app/src/main/java/com/philips/lighting/quickstart/MyApplicationActivity.java
@@ -1,5 +1,7 @@
 package com.philips.lighting.quickstart;
 
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -14,6 +16,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.philips.lighting.hue.listener.PHLightListener;
 import com.philips.lighting.hue.sdk.PHHueSDK;
@@ -24,21 +28,24 @@ import com.philips.lighting.model.PHLight;
 import com.philips.lighting.model.PHLightState;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+
 /**
- * MyApplicationActivity - The starting point for creating your own Hue App.  
+ * MyApplicationActivity - The starting point for creating your own Hue App.
  * Currently contains a simple view with a button to change your lights to random colours.  Remove this and add your own app implementation here! Have fun!
- * 
- * @author SteveyO
  *
+ * @author SteveyO
  */
-public class MyApplicationActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class MyApplicationActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private PHHueSDK phHueSDK;
-    private static final int MAX_HUE=65535;
+    private static final int MAX_HUE = 65535;
     public static final String TAG = "QuickStart";
     GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
     TextView latitudeText;
     TextView longitudeText;
-    
+    Location mCurrentLocation;
+    String mLastUpdateTime;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,19 +59,7 @@ public class MyApplicationActivity extends Activity implements GoogleApiClient.C
 
         buildGoogleApiClient();
         mGoogleApiClient.connect();
-
-//        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-//                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
-//                .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
-//                .addApi(LocationServices.API)
-//                .build();
-//        mGoogleApiClient.connect();
-
-//        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-//        if (mLastLocation != null){
-//            latitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
-//        }
-
+        createLocationRequest();
 
         randomButton = (Button) findViewById(R.id.buttonRand);
         randomButton.setOnClickListener(new OnClickListener() {
@@ -87,12 +82,19 @@ public class MyApplicationActivity extends Activity implements GoogleApiClient.C
                 .build();
     }
 
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(3000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
     public void randomLights() {
         PHBridge bridge = phHueSDK.getSelectedBridge();
 
         List<PHLight> allLights = bridge.getResourceCache().getAllLights();
         Random rand = new Random();
-        
+
         for (PHLight light : allLights) {
             PHLightState lightState = new PHLightState();
             lightState.setHue(rand.nextInt(MAX_HUE));
@@ -102,40 +104,45 @@ public class MyApplicationActivity extends Activity implements GoogleApiClient.C
             //  bridge.updateLightState(light, lightState);   // If no bridge response is required then use this simpler form.
         }
     }
+
     // If you want to handle the response from the bridge, create a PHLightListener object.
     PHLightListener listener = new PHLightListener() {
-        
+
         @Override
-        public void onSuccess() {  
+        public void onSuccess() {
         }
-        
+
         @Override
         public void onStateUpdate(Map<String, String> arg0, List<PHHueError> arg1) {
-           Log.w(TAG, "Light has updated");
+            Log.w(TAG, "Light has updated");
         }
-        
-        @Override
-        public void onError(int arg0, String arg1) {}
 
         @Override
-        public void onReceivingLightDetails(PHLight arg0) {}
+        public void onError(int arg0, String arg1) {
+        }
 
         @Override
-        public void onReceivingLights(List<PHBridgeResource> arg0) {}
+        public void onReceivingLightDetails(PHLight arg0) {
+        }
 
         @Override
-        public void onSearchComplete() {}
+        public void onReceivingLights(List<PHBridgeResource> arg0) {
+        }
+
+        @Override
+        public void onSearchComplete() {
+        }
     };
-    
+
     @Override
     protected void onDestroy() {
         PHBridge bridge = phHueSDK.getSelectedBridge();
         if (bridge != null) {
-            
+
             if (phHueSDK.isHeartbeatEnabled(bridge)) {
                 phHueSDK.disableHeartbeat(bridge);
             }
-            
+
             phHueSDK.disconnect(bridge);
             super.onDestroy();
         }
@@ -144,10 +151,13 @@ public class MyApplicationActivity extends Activity implements GoogleApiClient.C
     @Override
     public void onConnected(Bundle bundle) {
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLastLocation != null){
-            latitudeText.setText("(" + String.valueOf(mLastLocation.getLatitude()) + ", " + String.valueOf(mLastLocation.getLongitude()) + ")");
+        if (mLastLocation != null) {
+            latitudeText.setText("(" + String.valueOf(mLastLocation.getLatitude()) + ", "
+                    + String.valueOf(mLastLocation.getLongitude()) + ")");
             longitudeText.setText(" ");
         }
+        startLocationUpdates();
+
     }
 
     @Override
@@ -158,5 +168,21 @@ public class MyApplicationActivity extends Activity implements GoogleApiClient.C
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
 
+    }
+
+    public void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+    private void updateUI() {
+        latitudeText.setText("(" + String.valueOf(mCurrentLocation.getLatitude()) + ", " + String.valueOf(mCurrentLocation.getLongitude()) + ")");
+        longitudeText.setText(mLastUpdateTime);
     }
 }
